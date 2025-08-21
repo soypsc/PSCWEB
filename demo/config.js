c8c05b6eb49419cb4ddd99d696a14258639ec198
soypsc <info@soypsc.com>
2025-08-21 12:24:20 -0500

    Create config.js

---
+++ demo/config.js
@@ -0,0 +1,7 @@
+const config = {
+    // Pega aquí la URL de tu proyecto de Supabase
+    supabaseUrl: 'https://osvecadxqcvbgzdemvdl.supabase.co',
+
+    // Pega aquí tu clave anónima (public anon key)
+    supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9zdmVjYWR4cWN2Ymd6ZGVtdmRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3OTU5NDYsImV4cCI6MjA3MTM3MTk0Nn0.WYwthwMHsb5vNrj_0-iUNBYJf7nbxBuy0gvHZ6LEjBY'
+};
