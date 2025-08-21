0571d0bf90f2dd5d4b9fd499d8fc86e06d4edb5d
soypsc <info@soypsc.com>
2025-08-21 16:04:00 -0500

    Update config.js

--- demo/config.js
+++ demo/config.js
@@ -1,5 +1,5 @@
 const supabaseUrl = 'https://osvecadxqcvbgzdemvdl.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9zdmVvZGthY3ZiZ2RnemVtbCIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzI0MjY4Njc1LCJleHAiOjE3NTU4MDQ2NzV9.aU3k1BfG3jK2H3gJ-5f6H2N1qQ1Z-Q7s3';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9zdmVjYWR4cWN2Ymd6ZGVtdmRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3OTU5NDYsImV4cCI6MjA3MTM3MTk0Nn0.WYwthwMHsb5vNrj_0-iUNBYJf7nbxBuy0gvHZ6LEjBY';
 
 export {
     supabaseUrl,
