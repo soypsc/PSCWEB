660ee5625aa25a6efa1292a34add0157b771b902
soypsc <info@soypsc.com>
2025-08-21 14:48:13 -0500

    Update config.js

--- demo/config.js
+++ demo/config.js
@@ -1,5 +1,3 @@
-const config = {
-
 const supabaseUrl = 'https://osveodkacvbgdgzemdl.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9zdmVvZGthY3ZiZ2RnemVtbCIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzI0MjY4Njc1LCJleHAiOjE3NTU4MDQ2NzV9.aU3k1BfG3jK2H3gJ-5f6H2N1qQ1Z-Q7s3';
 
