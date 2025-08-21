05c48e9c4fb6d94bfef4dd28b580c559d4a63f56
soypsc <info@soypsc.com>
2025-08-21 14:46:48 -0500

    Update config.js

--- demo/config.js
+++ demo/config.js
@@ -1,7 +1,9 @@
 const config = {
-    // Pega aquí la URL de tu proyecto de Supabase
-    supabaseUrl: 'https://osveodkacvbgdgzemdl.supabase.co',
 
-    // Pega aquí tu clave anónima (public anon key)
-    supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9zdmVvZGthY3ZiZ2RnemVtbCIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzI0MjY4Njc1LCJleHAiOjE3NTU4MDQ2NzV9.aU3k1BfG3jK2H3gJ-5f6H2N1qQ1Z-Q7s3'
+const supabaseUrl = 'https://osveodkacvbgdgzemdl.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9zdmVvZGthY3ZiZ2RnemVtbCIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzI0MjY4Njc1LCJleHAiOjE3NTU4MDQ2NzV9.aU3k1BfG3jK2H3gJ-5f6H2N1qQ1Z-Q7s3';
+
+export {
+    supabaseUrl,
+    supabaseAnonKey
 };
