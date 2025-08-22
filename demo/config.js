283e1e7274be20aea0c2858392969a3562b6a715
soypsc <info@soypsc.com>
2025-08-21 21:41:24 -0500

    Update config.js

--- demo/config.js
+++ demo/config.js
@@ -1,8 +1,6 @@
-// config.js
 const supabaseUrl = 'https://osvecadxqcvbgzdemvdl.supabase.co';
-
-// ⚠️ Usa tu ANON KEY completa desde el dashboard (no la service_role)
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9zdmVjYWR4cWN2Ymd6ZGVtdmRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3OTU5NDYsImV4cCI6MjA3MTM3MTk0Nn0.WYwthwMHsb5vNrj_0-iUNBYJf7nbxBuy0gvHZ6LEjBY';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9zdmVjYWR4cWN2Ymd6ZGVtdmRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3OTU5NDYsImV4cCI6MjA3MTM3MTk0Nn0.WYwthwMHsb5vNrj_0-iUNBYJf7nbxBuy0gvHZ6LEjBY'; // copia tu anon key del dashboard
 
 export { supabaseUrl, supabaseAnonKey };
+
 
