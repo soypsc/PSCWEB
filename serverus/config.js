ce6cf5efc0872a0f92f374d53329685c0cd34455
soypsc <info@soypsc.com>
2025-08-23 01:54:51 -0500

    Update config.js

--- serverus/config.js
+++ serverus/config.js
@@ -1,8 +1,5 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>URL</key>
-	<string>https://github.com/mherrera31/portal-front-/blob/main/serverus/config.js</string>
-</dict>
-</plist>
+// config.js
+window.PSC_CONFIG = {
+  supabaseUrl: "https://ztdkaxbgpogudtrdgzic.supabase.co",   // 🔑 Reemplaza con tu URL
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0ZGtheGJncG9ndWR0cmRnemljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4NDE2MDYsImV4cCI6MjA3MDQxNzYwNn0.mpjY5nWiS48O0DAUkCL7Jl6agF6LurNGeaY8d1u70Po"                   // 🔑 Reemplaza con tu anon key
+};
