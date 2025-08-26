2e5af9143b3b7296fdcfefbda3e9e850adf31cf9
soypsc <info@soypsc.com>
2025-08-25 22:55:57 -0500

    Update and rename config.js to index.html

--- demo/config.js
+++
@@ -1,6 +0,0 @@
-const supabaseUrl = 'https://osvecadxqcvbgzdemvdl.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9zdmVjYWR4cWN2Ymd6ZGVtdmRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3OTU5NDYsImV4cCI6MjA3MTM3MTk0Nn0.WYwthwMHsb5vNrj_0-iUNBYJf7nbxBuy0gvHZ6LEjBY'; // copia tu anon key del dashboard
-
-export { supabaseUrl, supabaseAnonKey };
-
-
