15694222b31536f35e4ca6bc05e3c55d7fdffd3e
soypsc <info@soypsc.com>
2025-08-27 12:31:56 -0500

    Create config.js

---
+++ app/config.js
@@ -0,0 +1,5 @@
+// config.js
+window.PSC_CONFIG = {
+  supabaseUrl: "https://ztdkaxbgpogudtrdgzic.supabase.co",   // 🔑 Reemplaza con tu URL
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0ZGtheGJncG9ndWR0cmRnemljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4NDE2MDYsImV4cCI6MjA3MDQxNzYwNn0.mpjY5nWiS48O0DAUkCL7Jl6agF6LurNGeaY8d1u70Po"                   // 🔑 Reemplaza con tu anon key
+};
