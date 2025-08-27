d12b8f9830274a35a68d98df25d14ca9b0eccc2e
soypsc <info@soypsc.com>
2025-08-27 18:12:38 -0500

    Update config.js

--- rayo/config.js
+++ rayo/config.js
@@ -1,5 +1,5 @@
 // config.js
 window.PSC_CONFIG = {
   supabaseUrl: "https://ztdkaxbgpogudtrdgzic.supabase.co",   // 🔑 Reemplaza con tu URL
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0ZGtheGJncG9ndWR0cmRnemljIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NDg0MTYwNiwiZXhwIjoyMDcwNDE3NjA2fQ.Z2x5kt0PybVhBgDVojjIm9FXjGLX04LjIF28-SoQg-k"                   // 🔑 Reemplaza con tu anon key
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0ZGtheGJncG9ndWR0cmRnemljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4NDE2MDYsImV4cCI6MjA3MDQxNzYwNn0.mpjY5nWiS48O0DAUkCL7Jl6agF6LurNGeaY8d1u70Po"                   // 🔑 Reemplaza con tu anon key
 };
