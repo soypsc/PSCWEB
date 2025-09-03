f023c0baca2eeb35b4a811c249b16a4ac9ac5935
soypsc <info@soypsc.com>
2025-09-03 08:06:31 -0500

    Update service-worker.js

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,10 +1,9 @@
-// Contenido para service-worker.js
+// service-worker.js
 
 self.addEventListener('push', function(event) {
   const data = event.data.json();
   const options = {
     body: data.body,
-    // Puedes cambiar este ícono por el de tu logo si quieres
     icon: data.icon || 'https://static.wixstatic.com/media/f7f5e1_bd90ae68234f4ee9975e3140c4bb47ef~mv2.png',
     badge: data.badge || 'https://static.wixstatic.com/media/f7f5e1_bd90ae68234f4ee9975e3140c4bb47ef~mv2.png'
   };
