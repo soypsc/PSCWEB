8137b84c54564a3a2b664933d5277a6c6ca3ecaa
soypsc <info@soypsc.com>
2025-09-05 23:18:45 -0500

    Update service-worker.js

--- miapp/service-worker.js
+++ miapp/service-worker.js
@@ -1,13 +1,17 @@
 // service-worker.js
 
 self.addEventListener('push', function(event) {
+  // Intenta analizar los datos del push como JSON.
   const data = event.data.json();
+  
+  // Opciones para la notificación que se mostrará.
   const options = {
     body: data.body,
-    icon: data.icon || 'https://static.wixstatic.com/media/f7f5e1_bd90ae68234f4ee9975e3140c4bb47ef~mv2.png',
-    badge: data.badge || 'https://static.wixstatic.com/media/f7f5e1_bd90ae68234f4ee9975e3140c4bb47ef~mv2.png'
+    icon: data.icon || 'https://static.wixstatic.com/media/f7f5e1_a5d98b45aa6647c7a63a372f0ca2cd81~mv2.jpg', // Icono por defecto
+    badge: data.badge || 'https://static.wixstatic.com/media/f7f5e1_a5d98b45aa6647c7a63a372f0ca2cd81~mv2.jpg' // Badge por defecto
   };
 
+  // Muestra la notificación. El service worker se mantendrá activo hasta que la notificación se cierre.
   event.waitUntil(
     self.registration.showNotification(data.title, options)
   );
